72dd2121d1858c51d51fa9b1df751dc5b713bbda
AshFungor <evgenynarkhov2@gmail.com>
2024-02-08 22:07:19 +0300

    Added pulse headers, version 17.0 + glib as dep

--- conanfile.py
+++ conanfile.py
@@ -4,5 +4,9 @@ class SoundServer(ConanFile):
     version = "0.0"
     settings = ["os", "compiler", "arch", "build_type"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires = ["plog/1.1.10", "gtest/1.14.0", "protobuf/3.21.12"]
+    requires = [            \
+        "plog/1.1.10",      \
+        "gtest/1.14.0",     \
+        "protobuf/3.21.12", \
+        "glib/2.78.3"]
     tool_requires = ["protobuf/3.21.12"]
