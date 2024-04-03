cb57cd97bb52c8d304978b70b6fbb585ebc18559
AshFungor <evgenynarkhov2@gmail.com>
2024-04-03 16:49:04 +0000

    cross complilation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,31 +1,80 @@
 #!/bin/sh
 
-# Set yours (or run from root folder)
+set -e
+
+# Must be root of project (relative to highest CMakeLists.txt)
 ROOT_FOLDER="$PWD"
 
+# Output dir, adjusted to target platform
+BUILD_DIR="$ROOT_FOLDER/build"
+
+# Available arch targets
+ARCH_TARGETS=("arm64" "system" "x86_64")
+
+# default target (auto system profile)
+ARCH="system"
+
 help() {
-    echo "helper for building Sound Server. Usage: build [-c|configure] [-b|build]"
+    echo "helper for building Sound Server. Usage: [-c|configure] [-b|build] [-a:ARCH|architecture]"
+    echo "architecture may be either \"system\", \"arm64\" or \"x86_64\""
 }
 
 configure() {
+    echo "Writing to dir: $BUILD_DIR, target platform is $ARCH"
     echo "Configuring..."
-    cd "$ROOT_FOLDER/build" || mkdir build
-    conan -v || "Error: Conan was not found."
-    cd "$ROOT_FOLDER/build" && conan install "$ROOT_FOLDER" --output-folder="$ROOT_FOLDER/build" --build=missing
-    cd "$ROOT_FOLDER/build" && cmake "$ROOT_FOLDER" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE="$ROOT_FOLDER/build/conan_toolchain.cmake"
+    test -d "$BUILD_DIR" || mkdir -p "$BUILD_DIR"
+    echo "Checking Conan installation..." && test -n "$(whereis conan)"
+    # match arch for deps
+    if [ "$ARCH" = "system" ]; then
+        PROFILE=""
+        COMPILER=""
+    elif [ "$ARCH" = "x86_64" ]; then 
+        PROFILE="$ROOT_FOLDER/data/x86_64-profile.txt"
+        COMPILER_C="gcc"
+        COMPILER_CXX="g++"
+        SYSTEM="x86_64"
+    elif [ "$ARCH" = "arm64" ]; then
+        PROFILE="$ROOT_FOLDER/data/arm64-profile.txt"
+        COMPILER_C="aarch64-linux-gnu-gcc"
+        COMPILER_CXX="aarch64-linux-gnu-g++"
+        SYSTEM="arm64"
+    fi
+
+    test -n "$PROFILE" \
+        && conan install "$ROOT_FOLDER" --output-folder="$BUILD_DIR" --build=missing -pr $PROFILE \
+        || conan install "$ROOT_FOLDER" --output-folder="$BUILD_DIR" --build=missing
+
+    cd "$BUILD_DIR" && test -n "$SYSTEM"                                \
+        && cmake                                                        \
+            "$ROOT_FOLDER" -G                                           \
+            "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug                   \
+            -DCMAKE_TOOLCHAIN_FILE="$BUILD_DIR/conan_toolchain.cmake"   \
+            -DCMAKE_CROSSCOMPILING=TRUE                                 \
+            -DCMAKE_SYSTEM_PROCESSOR="$SYSTEM"                          \
+        || cmake                                                        \
+            "$ROOT_FOLDER" -G                                           \
+            "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug                   \
+            -DCMAKE_TOOLCHAIN_FILE="$BUILD_DIR/conan_toolchain.cmake"
 }
 
 build() {
+    test -d "$BUILD_DIR" || configure
     echo "Building..."
-    cd "$ROOT_FOLDER/build" || $(printf "Build floder does not exist, configuring..." && configure)
-    cmake --build "$ROOT_FOLDER/build"
+    cmake --build "$BUILD_DIR"
 }
-while getopts "hcb" arg; do 
-    if [ "$arg" = "c" ]; then
-        configure
+
+while getopts "hcba:" arg; do 
+    if [ "$arg" = "a" ]; then
+        test -n "$(echo $ARCH_TARGETS | grep "$OPTARG")" && ARCH="$OPTARG"
     elif [ "$arg" = "b" ]; then
+        BUILD_DIR="$BUILD_DIR"_"$ARCH"
         build
-    else 
+    elif [ "$arg" = "h" ]; then
         help
+    elif [ "$arg" = "c" ]; then
+        BUILD_DIR="$BUILD_DIR"_"$ARCH"
+        configure
+    else
+        return 1
     fi
 done
--- src/ssd/network/protocols/simple/protocol.hpp
+++ src/ssd/network/protocols/simple/protocol.hpp
@@ -8,12 +8,26 @@
 #include <protos/client/client-message.pb.h>
 #include <protos/client/simple/simple.pb.h>
 
+// laar
+#include <network/message.hpp>
+
 // msfsm
 #include <msfsm.hpp>
 
 
 namespace srv {
 
+    class IProtocol : msfsm::Fsm<IProtocol> {
+        virtual void start() = 0;
+        virtual void terminate() = 0;
+    };
 
+    class SimpleProtocol : IProtocol {
+        SimpleProtocol();
+
+    private:
+        NSound::NSimple::TSimpleMessage::TStreamConfiguration streamConfig_;
+
+    };
 
 }
