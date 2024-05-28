7db87ba5b0c0cff39a649dbe1e3c262a3b29d914
AshFungor <evgenynarkhov2@gmail.com>
2024-05-28 11:15:51 +0000

    making cross compilation

--- conanfile.py
+++ conanfile.py
@@ -9,5 +9,6 @@ class SoundServer(ConanFile):
         "gtest/1.14.0",
         "protobuf/3.21.12",
         "nlohmann_json/3.11.3",
-        "boost/1.84.0"]
+        "boost/1.84.0",
+        "libalsa/1.2.10"]
     tool_requires = ["protobuf/3.21.12"]
--- scripts/build.sh
+++ scripts/build.sh
@@ -8,15 +8,8 @@ ROOT_FOLDER="$PWD"
 # Output dir, adjusted to target platform
 BUILD_DIR="$ROOT_FOLDER/build"
 
-# Available arch targets
-ARCH_TARGETS=("arm64" "system" "x86_64")
-
-# default target (auto system profile)
-ARCH="system"
-
 help() {
-    echo "helper for building Sound Server. Usage: [-c|configure] [-b|build] [-a:ARCH|architecture]"
-    echo "architecture may be either \"system\", \"arm64\" or \"x86_64\""
+    echo "helper for building Sound Server. Usage: [-c|configure] [-b|build]"
 }
 
 configure() {
@@ -24,37 +17,11 @@ configure() {
     echo "Configuring..."
     test -d "$BUILD_DIR" || mkdir -p "$BUILD_DIR"
     echo "Checking Conan installation..." && test -n "$(whereis conan)"
-    # match arch for deps
-    if [ "$ARCH" = "system" ]; then
-        PROFILE=""
-        COMPILER=""
-    elif [ "$ARCH" = "x86_64" ]; then
-        PROFILE=""
-        COMPILER=""
-        # PROFILE="$ROOT_FOLDER/data/x86_64-profile.txt"
-        # COMPILER_C="gcc"
-        # COMPILER_CXX="g++"
-        # SYSTEM="x86_64"
-    elif [ "$ARCH" = "arm64" ]; then
-        PROFILE="$ROOT_FOLDER/data/arm64-profile.txt"
-        COMPILER_C="aarch64-linux-gnu-gcc"
-        COMPILER_CXX="aarch64-linux-gnu-g++"
-        SYSTEM="arm"
-    fi
 
-    test -n "$PROFILE" \
-        && conan install "$ROOT_FOLDER" --output-folder="$BUILD_DIR" --build=missing -pr $PROFILE \
-        || conan install "$ROOT_FOLDER" --output-folder="$BUILD_DIR" --build=missing
+    conan install "$ROOT_FOLDER" --output-folder="$BUILD_DIR" --build=missing $CONAN_ARGS
 
-    cd "$BUILD_DIR" && test -n "$SYSTEM"                                \
+    cd "$BUILD_DIR"                                                     \
         && cmake                                                        \
-            "$ROOT_FOLDER" -G                                           \
-            "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug                   \
-            -DCMAKE_TOOLCHAIN_FILE="$BUILD_DIR/conan_toolchain.cmake"   \
-            -DCMAKE_SYSTEM_NAME=Generic                                 \
-            -DCMAKE_SYSTEM_PROCESSOR="$SYSTEM"                          \
-            -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY              \
-        || cmake                                                        \
             "$ROOT_FOLDER" -G                                           \
             "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug                   \
             -DCMAKE_TOOLCHAIN_FILE="$BUILD_DIR/conan_toolchain.cmake"
@@ -67,15 +34,11 @@ build() {
 }
 
 while getopts "hcba:" arg; do 
-    if [ "$arg" = "a" ]; then
-        test -n "$(echo $ARCH_TARGETS | grep "$OPTARG")" && ARCH="$OPTARG"
-    elif [ "$arg" = "b" ]; then
-        test "$ARCH" = "system" && BUILD_DIR="$BUILD_DIR" || BUILD_DIR="$BUILD_DIR"_"$ARCH"
+    if [ "$arg" = "b" ]; then
         build
     elif [ "$arg" = "h" ]; then
         help
     elif [ "$arg" = "c" ]; then
-        test "$ARCH" = "system" && BUILD_DIR="$BUILD_DIR" || BUILD_DIR="$BUILD_DIR"_"$ARCH"
         configure
     else
         return 1
