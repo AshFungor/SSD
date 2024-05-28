3e2797404c722e3e9fcf40ab72dee6a8b0e0e587
AshFungor <evgenynarkhov2@gmail.com>
2024-05-28 23:12:37 +0000

    little progress

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,22 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
 
 class SoundServer(ConanFile):
     version = "0.0"
-    settings = ["os", "compiler", "arch", "build_type"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires = [
-        "plog/1.1.10",
-        "gtest/1.14.0",
-        "protobuf/3.21.12",
-        "nlohmann_json/3.11.3",
-        "boost/1.84.0",
-        "libalsa/1.2.10"]
+    settings = "os", "compiler", "build_type", "arch"
     tool_requires = ["protobuf/3.21.12"]
+
+    def requirements(self):
+        for req in [
+            "plog/1.1.10",
+            "gtest/1.14.0",
+            "protobuf/3.21.12",
+            "nlohmann_json/3.11.3",
+            "boost/1.84.0",
+            "libalsa/1.2.10"
+        ]:
+            self.requires(req)
+
+    def layout(self):
+        cmake_layout(self)
