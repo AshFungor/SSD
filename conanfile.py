df40f7f437dc0ba4aa1a24491f029e90b3d3c4ec
AshFungor <evgenynarkhov2@gmail.com>
2024-05-30 23:57:56 +0000

    cross compile ready

--- conanfile.py
+++ conanfile.py
@@ -5,15 +5,15 @@ class SoundServer(ConanFile):
     version = "0.0"
     generators = ["CMakeToolchain", "CMakeDeps"]
     settings = "os", "compiler", "build_type", "arch"
-    tool_requires = ["protobuf/3.21.12"]
+    tool_requires = ["protobuf/5.27.0"]
 
     def requirements(self):
         for req in [
             "plog/1.1.10",
             "gtest/1.14.0",
-            "protobuf/3.21.12",
+            "protobuf/5.27.0",
             "nlohmann_json/3.11.3",
-            "boost/1.84.0",
+            "boost/1.85.0",
             "libalsa/1.2.10"
         ]:
             self.requires(req)
